75e437499d74f5cae3736b6084695b76cec4647f
Sylwester Grzywna <sylwester.grzywna@gmail.com>
2019-03-09 22:00:29 +0100

    First working release

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # portforward
-Simple and small TCP port forwarder with SOCKS5 proxy support .
+
+Simple and small port forwarder with SOCKS5 proxy support.
+
+I'm not the original author of the source code, I've just put together some recipes found on the Internet.
+
+This simple application is an excellent example of the power and simplicity of go - fully functional port forwarder in just a few lines of the source code.
+
+## Usage examples
+
+### Direct port forwarding
+
+```bash
+portforward localhost:2222 192.168.0.1:22
+```
+
+### Port forwarding using SOCKS5 proxy
+
+```bash
+portforward -proxy localhost:1080 localhost:2222 192.168.0.1:22
+```
--- portforward.go
+++ portforward.go
@@ -1,52 +1,110 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+
+	"golang.org/x/net/proxy"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatal("usage: portforward local remote...")
+	flag.Usage = usage
+
+	proxyAddrString := flag.String("proxy", "", "SOCKS5 proxy address")
+
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(1)
 	}
-	localAddrString := os.Args[1]
-	remoteAddrString := os.Args[2]
+
+	localAddrString := flag.Arg(0)
+	remoteAddrString := flag.Arg(1)
+
 	localAddr, err := net.ResolveTCPAddr("tcp", localAddrString)
 	if localAddr == nil {
 		log.Fatalf("net.ResolveTCPAddr failed: %s", err)
 	}
+
 	local, err := net.ListenTCP("tcp", localAddr)
 	if local == nil {
 		log.Fatalf("portforward: %s", err)
 	}
-	log.Printf("portforward listen on %s", localAddr)
+
+	if *proxyAddrString == "" {
+		log.Printf("portforward listen on %s forward to %s", localAddr, remoteAddrString)
+	} else {
+		log.Printf("portforward listen on %s forward to %s through %s", localAddr, remoteAddrString, *proxyAddrString)
+	}
+
 	for {
 		conn, err := local.Accept()
 		if conn == nil {
 			log.Fatalf("accept failed: %s", err)
 		}
-		go forward(conn, remoteAddrString)
+		if *proxyAddrString == "" {
+			go directForward(conn, remoteAddrString)
+		} else {
+			go proxyForward(conn, *proxyAddrString, remoteAddrString)
+		}
 	}
 }
 
-func forward(local net.Conn, remoteAddr string) {
+func usage() {
+	fmt.Printf("Usage: %s [OPTIONS] localaddr:port remoteaddr:port\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func directForward(local net.Conn, remoteAddr string) {
 	remote, err := net.Dial("tcp", remoteAddr)
 	if remote == nil {
 		log.Printf("remote dial failed: %v\n", err)
 		local.Close()
 		return
 	}
+
 	go func() {
 		defer local.Close()
-		//remote.SetReadTimeout(120*1E9)
 		io.Copy(local, remote)
 	}()
+
 	go func() {
 		defer remote.Close()
-		//local.SetReadTimeout(120*1E9)
 		io.Copy(remote, local)
 	}()
+
+	log.Printf("forward %s to %s", local.RemoteAddr(), remoteAddr)
+}
+
+func proxyForward(local net.Conn, proxyAddrString, remoteAddr string) {
+	dialer, err := proxy.SOCKS5("tcp", proxyAddrString, nil, proxy.Direct)
+	if err != nil {
+		log.Printf("SOCKS5 proxy failed: %v\n", err)
+		local.Close()
+		return
+	}
+
+	remote, err := dialer.Dial("tcp", remoteAddr)
+	if remote == nil {
+		log.Printf("remote dial failed: %v\n", err)
+		local.Close()
+		return
+	}
+
+	go func() {
+		defer local.Close()
+		io.Copy(local, remote)
+	}()
+
+	go func() {
+		defer remote.Close()
+		io.Copy(remote, local)
+	}()
+
 	log.Printf("forward %s to %s", local.RemoteAddr(), remoteAddr)
 }
